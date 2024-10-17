19830780ecaeac509fcb16f0ac1ff6ef13dc385f
AnimishTemple <animishtenneti@gmail.com>
2024-10-17 11:37:01 -0400

    Custom fragment function being used to roll die

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,25 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+
+            (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
+            // returns an object of type fragment, the reference type so cast to a die fragment
+
+        }
+
     }
 }
